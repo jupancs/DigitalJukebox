2003a31ee34ff47dcf3d4d844f0b3ce67fa012a1
Wang Tian <wangtian@dhcp-10-142-202-205.uawifi.arizona.edu>
2019-03-27 20:14:25 -0700

    dynamic listview example

--- QueueList/MainActivity.java
+++ QueueList/MainActivity.java
@@ -9,11 +9,19 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
-    String[] songs = {"song1", "song2", "song3", "song4", "song5", "song6", "song7", "song8", "song9", "song10"};
+    EditText editText;
+    Button button;
+    List<String> songs;
+    String[] songs2 = {"song1", "song2", "song3", "song4", "song5", "song6", "song7", "song8", "song9", "song10"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +38,26 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
+        songs = new ArrayList<>();
+        for (int i=0; i<songs2.length; i++){
+            songs.add(songs2[i]);
+        }
         lv = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, songs);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, songs);
         lv.setAdapter(arrayAdapter);
+        editText = findViewById(R.id.text01);
+        editText.setEnabled(false);
+        button = findViewById(R.id.button01);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (songs.size() > 0){
+                    songs.remove(0);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 
     @Override
